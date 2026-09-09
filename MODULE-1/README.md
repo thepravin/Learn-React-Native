3425f8f0c1ecdf01d0ce19afb89d0b66843ca657
Pravin Nalawade <pravinonline05@gmail.com>
2026-09-09 15:59:24 +0530

    basic core components

---
+++ MODULE-1/README.md
@@ -0,0 +1,16 @@
+Essential core components
+- view
+- text
+- image
+- textinput
+- pressable
+- scrollview
+- styleSheet
+
+
+
+# view : 
+- used to wrap other components
+
+# Pressable :
+- detect various stages of press interactions on any of its defined children
---
+++ MODULE-1/app/index.tsx
@@ -0,0 +1,338 @@
+import {
+  Button,
+  Pressable,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+import React from "react";
+import { Image } from "expo-image";
+
+
+const index = () => {
+  return (
+    <ScrollView>
+      <Text>Hello</Text>
+      <Image
+        source={require("../assets/images/android-icon-monochrome.png")}
+        style={styles.image}
+        contentFit="contain"
+      />
+      <Image
+        source={{
+          uri: "https://tse3.mm.bing.net/th/id/OIP.4F7wau4U1BybrEbeUYVjlgAAAA?r=0&rs=1&pid=ImgDetMain&o=7&rm=3",
+        }}
+        style={{
+          width: 100,
+          height: 100,
+        }}
+      />
+
+        <Button
+        onPress={()=>alert("Button press")}
+        title="Button title"
+        color="#841584"
+        accessibilityLabel="Button accessibility Label"
+        />
+        
+
+      <TextInput
+        placeholder="enter your name please"
+        style={{
+          borderWidth: 1,
+          padding: 10,
+          margin: 20,
+         
+        }}
+      />
+
+      <Pressable onPress={() => alert("Button Pressed!")}
+        style={{
+           marginBottom: 40
+        }}>
+        <Text>Click Me</Text>
+      </Pressable>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+      <Text>Hello world</Text>
+    </ScrollView>
+  );
+};
+
+export default index;
+
+const styles = StyleSheet.create({
+  image: {
+    width: 100,
+    height: 100,
+  },
+});
