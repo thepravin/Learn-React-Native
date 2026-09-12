1593fb4f3dab2f31077212b4d0c7cc195aa6eed9
Pravin Nalawade <pravinonline05@gmail.com>
2026-09-12 11:27:51 +0530

    custome screen headers

--- MODULE-4-NAVIGATION/app/App.tsx
+++ MODULE-4-NAVIGATION/app/App.tsx
@@ -1,4 +1,5 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Pressable, Text } from "react-native";
 import About from "./screens/about";
 import Details from "./screens/details";
 import HOME from "./screens/home";
@@ -8,7 +9,33 @@ const Stack = createNativeStackNavigator();
 function RootStack() {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="home screen top" component={HOME} />
+      <Stack.Screen
+        name="home screen top"
+        component={HOME}
+        options={{
+          title: "My Home Title 🏡",
+          headerStyle: {
+            backgroundColor: "#222",
+          },
+          headerTitleStyle: {
+            fontWeight: "bold",
+            color: "#fff",
+          },
+          headerRight: () => (
+            <Pressable
+              onPress={() => alert("This is a header button...")}
+              style={{
+                backgroundColor: "#e10f0f",
+                paddingHorizontal: 12,
+                paddingVertical: 6,
+                borderRadius: 6,
+              }}
+            >
+              <Text style={{ color: "#ffffff", fontWeight: "bold" }}>Info</Text>
+            </Pressable>
+          ),
+        }}
+      />
       <Stack.Screen
         name="details screen top"
         component={Details}
