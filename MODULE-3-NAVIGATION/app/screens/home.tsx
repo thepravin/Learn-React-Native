dd0d6fe6de9585039b18921c50e152522acffe4b
Pravin Nalawade <pravinonline05@gmail.com>
2026-09-12 10:47:35 +0530

    useNavigation hook

--- MODULE-3-NAVIGATION/app/screens/home.tsx
+++ MODULE-3-NAVIGATION/app/screens/home.tsx
@@ -1,14 +1,20 @@
-import { Link } from "@react-navigation/native";
+import { useNavigation } from "@react-navigation/native";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Pressable, StyleSheet, Text } from "react-native";
 
 const home = () => {
+  const navigation = useNavigation<any>();
+
   return (
-    <View>
-      <Text>home component text</Text>
-      <Link screen={"details screen top"}>Go to details</Link>
-      <Link screen={"about"}>Go to about page</Link>
-    </View>
+    // <View>
+    //   <Text>home component text</Text>
+    //   <Link screen={"details screen top"}>Go to details</Link>
+    //   <Link screen={"about"}>Go to about page</Link>
+    // </View>
+
+    <Pressable onPress={() => navigation.navigate("details screen top")}>
+      <Text>Go to details with useNavigation</Text>
+    </Pressable>
   );
 };
 
